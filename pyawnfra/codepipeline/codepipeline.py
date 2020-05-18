c7b4475f78b3eec36a75641e89c58d612cb2e738
Guy Wilson <guywilsonjr@gmail.com>
2020-05-18 02:28:37 -0700

    reorganize pipeline code and secrets

--- pyawnfra/codepipeline/codepipeline.py
+++ pyawnfra/codepipeline/codepipeline.py
@@ -8,54 +8,6 @@ from aws_cdk import (
     aws_iam as iam,
     aws_s3 as s3,
 )
-
-
-"""
-iam:DeleteRole  TestAppPyAwnfraPipelineC9F231D6-BuildRole41B77417-1JDP0IG2BNARN
-#env:
-  #secrets-manager:
-     # key: secret-id:json-key:version-stage:version-id
-     # key: secret-id:json-key:version-stage:version-id
-  #exported-variables:
-     # - variable
-     # - variable
-  #git-credential-helper: yes
-
-phases:
-  install:
-    #If you use the Ubuntu standard image 2.0 or later, you must specify runtime-versions.
-    #If you specify runtime-versions and use an image other than Ubuntu standard image 2.0, the build fails.
-    runtime-versions:
-      # name: version
-      # name: version
-      python: 3.7
-    commands:
-       - yum install node
-       - npm install -g aws-cdk
-  #pre_build:
-    #commands:
-      # - command
-      # - command
-  build:
-    commands:
-      # - command
-      # - command
-  #post_build:
-    #commands:
-      # - command
-      # - command
-#artifacts:
-  #files:
-    # - location
-    # - location
-  #name: $(date +%Y-%m-%d)
-  #discard-paths: yes
-  #base-directory: location
-#cache:
-  #paths:
-    # - paths
-"""
-
 ACCOUNT_ID = core.Aws.ACCOUNT_ID
 PARTITION = core.Aws.PARTITION
 REGION = core.Aws.REGION
--- pyawnfra/secretsmanager/secrets.py
+++ pyawnfra/secretsmanager/secrets.py
@@ -1,15 +1,17 @@
-from aws_cdk import core, aws_secretsmanager as sm
+from aws_cdk import core,aws_kms as kms, aws_secretsmanager as sm
 
 
 class SecretStack(core.Stack):
     def __init__(self, app: core.App, id: str, secret_placeholders: list) -> None:
         super().__init__(app, id)
+
+        self.kms_key = kms.Key(self, "KMSKey")
         self.create_placeholders(secret_placeholders)
 
     def create_placeholders(self, secret_placeholders: list) -> None:
         self.secrets = []
         for secret in secret_placeholders:
-            secret_obj = sm.Secret(self, secret, secret_name=secret)
+            secret_obj = sm.Secret(self, secret, encryption_key=self.kms_key, secret_name=secret)
             self.secrets.append(secret_obj)
 
         self.secret_names = secret_placeholders
